5ef8015e4c160e2da2b3264007b57c04189d7f4a
stdp <>
2021-06-10 20:31:43 +1000

    - adding working basic example of learning and recognising

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # Learning how to use the Akida with a Camera Feed
 
+This is an extremely basic way to utilise the Akida on-chip learning functionality. The demo will let you learn new classes of objects to recognise in the camera feed.
+
 ## Setting up the Akida development evironment
 
 1. Go to `https://www.anaconda.com/download/` and download intaller
@@ -7,5 +9,14 @@
 3. Create conda environment `conda create --name akida_env python=3.6`
 4. Activate conda environement `conda activate akida_env`
 5. Install python dependencies `pip install -r requirements.txt`
-6. Follow the directions in the python scripts
 
+## Running and using the example
+
+1. `python3 akida_camera.py`
+2. Press `1`, `2` or `3` to save learn a new class
+3. Press `s` to save the newly learnt features into your model
+
+
+### Read More
+
+Read all the documentation at https://doc.brainchipinc.com
--- akida_camera.py
+++ akida_camera.py
@@ -1,17 +1,21 @@
-from imutils.video import WebcamVideoStream
+from imutils.video import VideoStream
 import numpy as np
-import cv2
 import os
-from tensorflow.keras.preprocessing.image import img_to_array, smart_resize
+import cv2
+from threading import Thread
+from tensorflow.keras.preprocessing.image import img_to_array
 import tensorflow_datasets as tfds
+
 from akida_models import mobilenet_edge_imagenet_pretrained
 from cnn2snn import convert
 from akida import Model, FullyConnected
 
+import time
 
 MODEL_FBZ = "models/edge_learning_example.fbz"
 
 CAMERA_SRC = 0
+CAMERA_FPS = 30
 
 NUM_NEURONS_PER_CLASS = 1
 NUM_WEIGHTS = 350
@@ -21,13 +25,38 @@ TARGET_WIDTH = 224
 TARGET_HEIGHT = 224
 
 
+class Controls:
+    def __init__(self, inference):
+        self.name = "keyboard_controls"
+        self.inference = inference
+
+    def capture(self):
+        if cv2.waitKey(33) == ord("1"):
+            print("learn 0")
+            self.inference.learn(0)
+
+        if cv2.waitKey(33) == ord("2"):
+            print("learn 1")
+            self.inference.learn(1)
+
+        if cv2.waitKey(33) == ord("3"):
+            print("learn 2")
+            self.inference.learn(2)
+
+        if cv2.waitKey(33) == ord("s"):
+            print("saving model")
+            self.inference.save()
+
+
 class Camera:
     def __init__(self):
-        self.stream = WebcamVideoStream(src=CAMERA_SRC).start()
+        self.stream = VideoStream(
+            src=CAMERA_SRC, resolution=(TARGET_WIDTH, TARGET_HEIGHT)
+        ).start()
 
     def get_frame(self):
         frame = self.stream.read()
-        frame = smart_resize(frame, (TARGET_WIDTH, TARGET_HEIGHT))
+        frame = cv2.resize(frame, (TARGET_WIDTH, TARGET_HEIGHT))
         return frame
 
     def get_input_array(self):
@@ -36,20 +65,26 @@ class Camera:
         input_array = np.array([input_array], dtype="uint8")
         return input_array
 
+    def show_frame(self):
+        frame = self.get_frame()
+        cv2.imshow("frame", frame)
+        key = cv2.waitKey(1) & 0xFF
+
 
 class Inference:
-    def __init__(self):
-        self.camera = Camera()
-        self.model_ak = Model(filename=MODEL_FBZ)
-
+    def __init__(self, camera):
+        # init the camera
+        self.camera = camera
         # create a model if one doesnt exist
         if not os.path.exists(MODEL_FBZ):
+            print("Initialising Akida model")
             self.initialise()
+        # load the akida model
+        self.model_ak = Model(filename=MODEL_FBZ)
 
     def initialise(self):
         ds, ds_info = tfds.load("coil100:2.*.*", split="train", with_info=True)
         model_keras = mobilenet_edge_imagenet_pretrained()
-
         # convert it to akida
         model_ak = convert(model_keras, input_scaling=(128, 128))
 
@@ -64,18 +99,29 @@ class Inference:
         model_ak.compile(
             num_weights=NUM_WEIGHTS, num_classes=NUM_CLASSES, learning_competition=0.1
         )
+        model_file = os.path.join("", MODEL_FBZ)
+        model_ak.save(model_file)
 
     def learn(self, neuron):
         input_array = self.camera.get_input_array()
         self.model_ak.fit(input_array, neuron)
 
+    def save(self):
+        model_file = os.path.join("", MODEL_FBZ)
+        self.model_ak.save(model_file)
+
     def infer(self):
         input_array = self.camera.get_input_array()
         predictions = self.model_ak.predict(input_array, num_classes=NUM_CLASSES)
-        print(predictions)
+        print(predictions[0])
 
 
-inference = Inference()
+camera = Camera()
+inference = Inference(camera)
+controls = Controls(inference)
 
 while True:
+    controls.capture()
+    camera.show_frame()
     inference.infer()
+    time.sleep(1 / CAMERA_FPS)
