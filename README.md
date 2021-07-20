303a84f5647a9021f259f9dc2e8fbe1ecfd6dd9b
stdp <stdp@users.noreply.github.com>
2021-07-20 23:33:11 +0800

    - cleaning up a bunch of logic - adding labels and # of shots - improving readme - linking to useful BrainChip content

--- README.md
+++ README.md
@@ -1,20 +1,49 @@
 # Learning how to use Akida with a Camera Feed
 
-This is an extremely basic way to utilise the Akida on-chip learning functionality. The demo will let you learn new classes of objects to recognise in the camera feed.
+This is an extremely basic way to utilise the Akida on-chip learning functionality. The demo will let you learn new classes of objects to recognise in the camera feed. This application is built to soley demonstrate how easy it is to use Akida's unique one-shot/few shot learning abilities.
 
-## Setting up the Akida development evironment
 
-1. Go to `https://www.anaconda.com/download/` and download installer
-2. Install anaconda `bash Anaconda-latest-Linux-x86_64.sh`
-3. Create conda environment `conda create --name akida_env python=3.6`
-4. Activate conda environement `conda activate akida_env`
-5. Install python dependencies `pip install -r requirements.txt`
+## How Does Akida Learn?
 
-## Running and using the example
+
+In  native  learning  mode,  event  domain  neurons  learn quickly through a biological process known as Spike Time Dependent Plasticity (STDP), in which synapses that match an activation pattern are reinforced. BrainChip is utilizing a naturally homeostatic form of STDP learning in which neurons don’t saturate or switch off completely. 
+
+STDP  is  possible  because  of  the  event-based processing method used by the Akida processor, and can be applied to incremental learning and one-shot or multi-shot learning. 
+
+**Read more:**
+
+[What Is the Akida Event Domain Neural Processor?](https://brainchipinc.com/what-is-the-akida-event-domain-neural-processor/)
+
+[MetaTF Documentation](https://doc.brainchipinc.com)
+
+
+### Setting up the Akida development evironment
+
+1. Go to `https://www.anaconda.com/download/` and download the installer installer
+2. Install Anaconda by running `bash Anaconda-latest-Linux-x86_64.sh`
+3. Once installed, create a conda environment `conda create --name akida_env python=3.6`
+4. Activate the new conda environement `conda activate akida_env`
+5. Install the python dependencies `pip install -r requirements.txt`
+
+
+### Running and using the app
 
 1. run `python3 akida_camera.py`
 2. Press `1` to `0` on your keyboard to learn a new class
 3. Press `s` to save the newly learnt classes into your model
+
+
+### Customise the labels
+
+1. Edit the dict `LABELS = {0: "Background"}`
+2. Add as many labels as required, ensure there are enough classes available in the model `NUM_CLASSES = 10`
+
+
+### Record a demonstration
+
+1. Change the output setting `OUTPUT = True`
+2. Enter a filename if required, defaults to `out.avi`
+> **Important:** Remember to contact `sales@brainchipinc.com` to seek permission before publishing any demonstration videos
 
 
 ## One-Shot learning as seen in the BrainChip Inc demo
@@ -23,7 +52,8 @@ Essentially this is a homemade version of this demonstration that BrainChip has 
 
 [![One-Shot Learning](http://img.youtube.com/vi/xeGAiWbKa7s/0.jpg)](https://youtu.be/xeGAiWbKa7s "One-Shot Learning")
 
+[![Akida, how do you like them apples?](http://img.youtube.com/vi/p9pXN5-opGw/0.jpg)](https://www.youtube.com/watch?v=p9pXN5-opGw "Akida, how do you like them apples?")
 
-### Read More
 
-Read all the documentation at https://doc.brainchipinc.com
+View more One-shot / few shot learning demonstration videos: 
+[User Community Platform](https://www.youtube.com/playlist?list=PLKZ8TPx-mIt2Mu3kXxm9BIW08lIDbvZdA)
--- akida_camera.py
+++ akida_camera.py
@@ -4,7 +4,6 @@ import cv2
 import threading
 from imutils.video import VideoStream
 from tensorflow.keras.preprocessing.image import img_to_array
-import tensorflow_datasets as tfds
 import numpy as np
 from pynput import keyboard
 
@@ -13,10 +12,14 @@ from cnn2snn import convert
 from akida import Model, FullyConnected
 
 
+OUTPUT = False
+OUTPUT_VID = "out.avi"
+OUTPUT_FPS = 30
+
 MODEL_FBZ = "models/edge_learning_example.fbz"
 
 CAMERA_SRC = 0
-INFERENCE_PER_SECOND = 2
+INFERENCE_PER_SECOND = 1
 
 TEXT_COLOUR = (190, 30, 255)
 
@@ -31,58 +34,27 @@ TARGET_WIDTH = 224
 TARGET_HEIGHT = 224
 
 NEURON_KEYS = [str(i) for i in range(10)]
-SAVE_BUTTON = 's'
+SAVE_BUTTON = "s"
 
-
-"""
-Method to initialise an Akida model if one doesn't exist
-"""
-
-
-def initialise():
-    ds, ds_info = tfds.load("coil100:2.*.*", split="train", with_info=True)
-    model_keras = mobilenet_edge_imagenet_pretrained()
-    # convert it to akida
-    model_ak = convert(model_keras, input_scaling=(128, 128))
-
-    # remove the last layer of network, replace with Akida learning layer
-    model_ak.pop_layer()
-    layer_fc = FullyConnected(
-        name="akida_edge_layer",
-        num_neurons=NUM_CLASSES * NUM_NEURONS_PER_CLASS,
-        activations_enabled=False,
-    )
-    # add learning layer to end of model
-    model_ak.add(layer_fc)
-    model_ak.compile(
-        num_weights=NUM_WEIGHTS, num_classes=NUM_CLASSES, learning_competition=0.1
-    )
-    # save new model
-    model_ak.save(MODEL_FBZ)
-
-
-# create a model if one doesnt exist
-if not os.path.exists(MODEL_FBZ):
-    print("Initialising Akida model")
-    initialise()
-
-
-"""
-Class to capture key presses to save/learn
-"""
+LABELS = {0: "Background"}
+SAVED = []
+SHOTS = {}
 
 
 class Controls:
 
+    """
+    Class to capture key presses to save/learn
+    """
+
     def __init__(self, inference):
         self.listener = keyboard.Listener(
-            on_press=self.on_press,
-            on_release=self.on_release)
+            on_press=self.on_press, on_release=self.on_release
+        )
         self.listener.start()
         self.inference = inference
 
     def on_press(self, key):
-        global inference
         try:
 
             if key.char in NEURON_KEYS:
@@ -101,17 +73,26 @@ class Controls:
             return False
 
 
-"""
-Class to capture video feed from webcam
-"""
-
-
 class Camera:
+
+    """
+    Class to capture video feed from webcam
+    """
+
     def __init__(self):
         self.stream = VideoStream(
             src=CAMERA_SRC, resolution=(FRAME_WIDTH, FRAME_HEIGHT)
         ).start()
-        self.label = 0
+        self.label = ""
+        self.shots = ""
+        self.text_display_timer = 0
+        if OUTPUT:
+            self.out = cv2.VideoWriter(
+                OUTPUT_VID,
+                cv2.VideoWriter_fourcc("M", "J", "P", "G"),
+                OUTPUT_FPS,
+                (FRAME_WIDTH, FRAME_HEIGHT),
+            )
 
     def get_frame(self):
         frame = cv2.resize(self.stream.read(), (TARGET_WIDTH, TARGET_HEIGHT))
@@ -125,51 +106,103 @@ class Camera:
 
     def show_frame(self):
         frame = self.label_frame(self.stream.read())
+        if OUTPUT:
+            self.out.write(frame)
         cv2.imshow("frame", frame)
-        key = cv2.waitKey(1) & 0xFF
+        key = cv2.waitKey(20) & 0xFF
 
     def label_frame(self, frame):
         frame = cv2.putText(
             frame,
             str(self.label),
-            (10, 30),
+            (10, 40),
             cv2.FONT_HERSHEY_SIMPLEX,
-            1,
+            1.2,
             TEXT_COLOUR,
-            5,
+            1,
+            cv2.LINE_AA,
+        )
+        frame = cv2.putText(
+            frame,
+            str(self.shots),
+            (10, 75),
+            cv2.FONT_HERSHEY_SIMPLEX,
+            0.7,
+            TEXT_COLOUR,
+            1,
             cv2.LINE_AA,
         )
         return frame
 
-    def set_label(self, label):
-        self.label = label
-
-
-"""
-Class to run inference over frames from the webcam
-"""
-
 
 class Inference:
+
+    """
+    Class to run inference over frames from the webcam
+    """
+
     def __init__(self, camera):
-        # init the camera
+
+        # create a model if one doesnt exist
+        if not os.path.exists(MODEL_FBZ):
+            print("Initialising Akida model")
+            self.initialise()
+
         self.camera = camera
+
         # run inference in separate thread
         self.t1 = threading.Thread(target=self.infer)
         self.t1.start()
+
         # load the akida model
         self.model_ak = Model(filename=MODEL_FBZ)
+
+    def initialise(self):
+
+        """
+        Method to initialise an Akida model if one doesn't exist
+        """
+
+        # fetch pretrained imagenet
+        model_keras = mobilenet_edge_imagenet_pretrained()
+
+        # convert it to an Akida model
+        model_ak = convert(model_keras, input_scaling=(128, 128))
+
+        # remove the last layer of network, replace with Akida learning layer
+        model_ak.pop_layer()
+        layer_fc = FullyConnected(
+            name="akida_edge_layer",
+            num_neurons=NUM_CLASSES * NUM_NEURONS_PER_CLASS,
+            activations_enabled=False,
+        )
+        # add learning layer to end of model
+        model_ak.add(layer_fc)
+        model_ak.compile(
+            num_weights=NUM_WEIGHTS, num_classes=NUM_CLASSES, learning_competition=0.1
+        )
+        # save new model
+        model_ak.save(MODEL_FBZ)
 
     def infer(self):
         while True:
             input_array = camera.get_input_array()
             predictions = self.model_ak.predict(input_array, num_classes=NUM_CLASSES)
-            self.camera.set_label(predictions[0])
+            if predictions[0] in SAVED:
+                self.camera.label = LABELS.get(predictions[0], predictions[0])
+                self.camera.shots = "{} shot/s".format(SHOTS.get(predictions[0]))
             time.sleep(1 / INFERENCE_PER_SECOND)
 
     def learn(self, neuron):
+        if neuron not in SAVED:
+            SAVED.append(neuron)
+            SHOTS[neuron] = 1
+        else:
+            SHOTS[neuron] += 1
+
         input_array = self.camera.get_input_array()
         self.model_ak.fit(input_array, neuron)
+        self.camera.set_label("Learned {}".format(LABELS.get(neuron, neuron)))
 
     def save(self):
         self.model_ak.save(MODEL_FBZ)
@@ -179,6 +212,6 @@ camera = Camera()
 inference = Inference(camera)
 controls = Controls(inference)
 
-# main loop to display camera feed
+
 while True:
     camera.show_frame()
